49a5dc62f6b5c012babb1be9c45bfe9762a4cfdf
Evgeny Nagaev <evn1054@gmail.com>
2022-10-16 21:46:56 +0300

    feat: solve the extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,62 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options ) {
+  // throw new NotImplementedError('Not implemented');
+  let srtArr = [], // финальный массив, который уже будет превращён в финальую строку
+      subStrArr = [], // это массив для формирования подстроки subStr, если addition есть 
+      subStr = '', // это подстрока которую выводит функция setSubString
+      finalStr = '', // это финальная строка, она формируется из финально массива srtArr и выводится в главный return
+      optionObj = {  // это объект, который мы заполняем на основе входящего options
+        repeatTimes: 1,
+        separator: '+',
+        addition: '',
+        additionRepeatTimes: 1,
+        additionSeparator: '|',
+      };
+
+    // определяем repeatTimes
+    if (options.repeatTimes) {
+      optionObj.repeatTimes = options.repeatTimes;
+    } 
+
+    // определяем separator
+    if (options.separator) {
+      optionObj.separator = options.separator;
+    } 
+    
+    // определяем addition
+    if (options.hasOwnProperty('addition')) {
+      if (String(options.addition)) {
+        optionObj.addition = options.addition;
+      } 
+    }
+
+    // определяем additionRepeatTimes
+    if (options.additionRepeatTimes) {
+      optionObj.additionRepeatTimes = options.additionRepeatTimes;
+    } 
+
+    // определяем additionSeparator
+    if (options.additionSeparator) {
+      optionObj.additionSeparator = options.additionSeparator;
+    } 
+
+    // цикл для формирования подстроки
+  for (let i = 1; i <= optionObj.additionRepeatTimes; i++) {
+    (i == optionObj.additionRepeatTimes) ? subStrArr.push(String(optionObj.addition)) : subStrArr.push(optionObj.addition + optionObj.additionSeparator);
+  }
+  subStr = subStrArr.join(''); // подстрока
+
+  // цикл для формирования финального массива из которого будет получена финальная строка
+  for (let i = 1; i <= optionObj.repeatTimes; i++) {
+    (i == optionObj.repeatTimes) ? srtArr.push(str + subStr) : srtArr.push(str + subStr + optionObj.separator);
+  }
+  finalStr = srtArr.join('');
+
+
+  return finalStr;
+
 }
 
 module.exports = {
